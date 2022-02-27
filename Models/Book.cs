1246985268a9d9815104bfc47068aee2a388f3bc
Kamil Brodziak <kamil98brodziak@gmail.com>
2022-02-27 22:37:34 +0100

    Point 1, 2a, 2b

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,21 +9,26 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-		[Required]
-		[StringLength(255)]
+		[Required(ErrorMessage = "Book name is required.")]
+		[StringLength(255, ErrorMessage = "Book name cannot be longer than 255 characters.")]
 		public string Name { get; set; }
-
-		[Required]
+		[Display(Name ="Author name")]
+		[Required(ErrorMessage = "Author name is required.")]
 		public string AuthorName { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Genre is required.")]
 		public Genre Genre { get; set; }
-		[Required]
-		[Display(Name = "Genre")]
 		public byte GenreId { get; set; }
+		[Display(Name="Date added")]
 		public DateTime DateAdded { get; set; }
-		[Display(Name="Realease Date")]
+		[Display(Name="Release date")]
+		[Required(ErrorMessage = "Release date is required.")]
 		public DateTime ReleaseDate { get; set; }
+		[Display(Name="Number in stock")]
+		[Required(ErrorMessage = "Number in stock is required.")]
+		[Range(1, 20, ErrorMessage = "Number in stock should be between 1 and 20.")]
 		public int NumberInStock { get; set; }
+		[Display(Name="Number available")]
+		[Required(ErrorMessage = "Number available is required.")]
 		public int NumberAvailable { get; set; }
 	}
       
--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -13,17 +13,24 @@ namespace LibApp.Models
             using (var context = new ApplicationDbContext(
                 serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
             {
-                if (context.MembershipTypes.Any())
-                {
-                    Console.WriteLine("Database already seeded");
-                    return;
-                }
+                SeedMembershipTypes(context);
+                SeedCustomers(context);
+                SeedBooks(context);
 
+                context.SaveChanges();
+            }
+        }
+
+        private static void SeedMembershipTypes(ApplicationDbContext context)
+        {
+            if(!context.MembershipTypes.Any())
+            {
                 context.MembershipTypes.AddRange(
                     new MembershipType
                     {
                         Id = 1,
                         SignUpFee = 0,
+                        Name = "Pay as you go",
                         DurationInMonths = 0,
                         DiscountRate = 0
                     },
@@ -31,6 +38,7 @@ namespace LibApp.Models
                     {
                         Id = 2,
                         SignUpFee = 30,
+                        Name = "Monthly",
                         DurationInMonths = 1,
                         DiscountRate = 10
                     },
@@ -38,6 +46,7 @@ namespace LibApp.Models
                     {
                         Id = 3,
                         SignUpFee = 90,
+                        Name = "Quaterly",
                         DurationInMonths = 3,
                         DiscountRate = 15
                     },
@@ -45,10 +54,63 @@ namespace LibApp.Models
                     {
                         Id = 4,
                         SignUpFee = 300,
+                        Name = "Yearly",
                         DurationInMonths = 12,
                         DiscountRate = 20
                     });
-                context.SaveChanges();
+            }
+            
+        }
+
+        private static void SeedCustomers(ApplicationDbContext context)
+        {
+            if (!context.Customers.Any())
+            {
+                context.Customers.AddRange(
+                new Customer
+                {
+                    Name = "Kamil Brodziak",
+                    HasNewsletterSubscribed = false,
+                    MembershipTypeId = 1,
+                    Birthdate = new DateTime(1998, 09, 18)
+                },
+                new Customer
+                {
+                    Name = "Janusz Januszowy",
+                    HasNewsletterSubscribed = true,
+                    MembershipTypeId = 2,
+                    Birthdate = new DateTime(2001, 06, 25)
+                });
+                Console.WriteLine("Customers already seeded");
+            }
+        }
+
+        private static void SeedBooks(ApplicationDbContext context)
+        {
+            if (!context.Books.Any())
+            {
+                context.Books.AddRange(
+                new Book
+                {
+                    Name = "Wiedźmin: Krew Elfów",
+                    AuthorName = "Andrzej Sapkowski",
+                    GenreId = 6,
+                    DateAdded = new DateTime(2022, 02, 27),
+                    ReleaseDate = new DateTime(1994, 02, 25),
+                    NumberInStock = 15,
+                    NumberAvailable = 4,
+                },
+                new Book
+                {
+                    Name = "Lord of the rings: The Return of the King",
+                    AuthorName = "J.R.R. Tolkien",
+                    GenreId = 6,
+                    DateAdded = new DateTime(2022, 02, 11),
+                    ReleaseDate = new DateTime(1955, 10, 20),
+                    NumberInStock = 17,
+                    NumberAvailable = 12,
+                });
+                Console.WriteLine("Books already seeded");
             }
         }
     }
